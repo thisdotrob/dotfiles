204561c966043d46892c2a8df2a5b79f61998e09
thisdotrob <robstevenson000@gmail.com>
2017-11-14 12:47:04 +0000

    Update README, make symlinks overwrite orig. files

--- README.md
+++ README.md
@@ -41,13 +41,12 @@
 1. Install command syntax highlighting and enable it:
    ```sh
    brew install zsh-syntax-highlighting
-   echo "source /usr/local/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ~/.zshrc
    source ~/.zshrc
    ```
 
 1. Install z:
    ```sh
-   git clone git@github.com:rupa/z.git ~/z && echo ". ~/z/z.sh" >> ~/.zshrc
+   git clone git@github.com:rupa/z.git ~/z
    source ~/.zshrc
    ```
 
--- makesymlinks.sh
+++ makesymlinks.sh
@@ -1,8 +1,8 @@
-   ln -s ~/dotfiles/.zshrc ~/.zshrc
-   ln -s ~/dotfiles/.vimrc ~/.vimrc
-   ln -s ~/dotfiles/.atom/config.cson ~/.atom/config.cson
-   ln -s ~/dotfiles/.atom/init.coffee ~/.atom/init.coffee
-   ln -s ~/dotfiles/.atom/keymap.cson ~/.atom/keymap.cson
-   ln -s ~/dotfiles/.atom/snippets.cson ~/.atom/snippets.cson
-   ln -s ~/dotfiles/.atom/styles.less ~/.atom/styles.less
-   ln -s ~/dotfiles/.atom/.apm/.apmrc ~/.atom/.apm/.apmrc
+   ln -sf ~/dotfiles/.zshrc ~/.zshrc
+   ln -sf ~/dotfiles/.vimrc ~/.vimrc
+   ln -sf ~/dotfiles/.atom/config.cson ~/.atom/config.cson
+   ln -sf ~/dotfiles/.atom/init.coffee ~/.atom/init.coffee
+   ln -sf ~/dotfiles/.atom/keymap.cson ~/.atom/keymap.cson
+   ln -sf ~/dotfiles/.atom/snippets.cson ~/.atom/snippets.cson
+   ln -sf ~/dotfiles/.atom/styles.less ~/.atom/styles.less
+   ln -sf ~/dotfiles/.atom/.apm/.apmrc ~/.atom/.apm/.apmrc
