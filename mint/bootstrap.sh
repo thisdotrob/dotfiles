44e7e2a5c0f9a39c8b344e54ad779de44e21d728
thisdotrob <rs@gower.st>
2018-07-27 18:08:12 +0100

    Update mint bootstrap

--- mint/bootstrap.sh
+++ mint/bootstrap.sh
@@ -1,40 +1,45 @@
 #!/bin/bash
-sudo apt add-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 
 sudo apt update
-
 sudo apt upgrade -y
 
+# git
+sudo apt install -y git
+ln -sf "$(pwd)/gitconfig" ~/.gitconfig
+
+# docker
+sudo apt add-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt update
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add
-
 sudo apt install -y apt-transport-https ca-certificates software-properties-common curl
-
 sudo apt install -y docker.io
-
 sudo usermod -aG docker $USER
-
 sudo curl -L https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 
-sudo apt install -y zsh zsh-syntax-highlighting
-
+# shell & term
+sudo apt install -y zsh zsh-syntax-highlighting gnome-terminal
 git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
-
-sudo apt install -y vim-gtk3 i3-wm i3status tmux htop libanyevent-i3-perl jq chromium-browser
-
 git clone https://github.com/sindresorhus/pure.git /tmp/pure
-
 sudo mv /tmp/pure/pure.zsh /usr/local/share/zsh/site-functions/prompt_pure_setup
 sudo mv /tmp/pure/async.zsh /usr/local/share/zsh/site-functions/async
-
 sudo rm -rf /tmp/pure
+ln -sf "$(pwd)/zshrc" ~/.zshrc
 
+# i3
+sudo apt install -y i3-wm i3status dmenu nm-applet libanyevent-i3-perl
 mkdir -p ~/.config/i3
 mkdir -p ~/.config/i3status
-
-ln -sf "$(pwd)/gitconfig" ~/.gitconfig
-ln -sf "$(pwd)/zshrc" ~/.zshrc
-ln -sf "$(pwd)/vimrc" ~/.vimrc
 ln -sf "$(pwd)/i3config" ~/.config/i3/config
 ln -sf "$(pwd)/i3statusconfig" ~/.config/i3status/config
+
+# vim
+sudo apt install -y vim-gtk3
+ln -sf "$(pwd)/vimrc" ~/.vimrc
+mkdir -p "~/.vim/pack/$USER/start"
+git submodule init
+ln -sf "$(pwd)/../vim-plugins" "~/.vim/pack/$USER/start"
+
+# apps
+sudo apt install -y tmux htop jq chromium-browser
 
 chsh -s /bin/zsh
