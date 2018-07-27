1ab2eb41b56c6851d749754921784f118394b4a1
thisdotrob <rs@gower.st>
2018-07-27 14:51:39 +0100

    Update make-links.sh

--- mint/make-links.sh
+++ mint/make-links.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
+apt add-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+	$(lsb_release -cs) \
+	stable"
+
+apt update
+
+apt install zsh vim-gtk3 curl i3-wm i3status tmux htop libanyevent-i3-perl jq docker-ce
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+mkdir -p ~/.config/i3
+mkdir -p ~/.config/i3status
+
 ln -sf "$(pwd)/zshrc" ~/.zshrc
 ln -sf "$(pwd)/vimrc" ~/.vimrc
-mkdir -p ~/.config/i3
 ln -sf "$(pwd)/i3config" ~/.config/i3/config
-mkdir -p ~/.config/i3status
 ln -sf "$(pwd)/i3statusconfig" ~/.config/i3status/config
