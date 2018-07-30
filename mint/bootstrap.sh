d251c42ad243dc83037c8dbefa81297b9f7aff1b
thisdotrob <rs@gower.st>
2018-07-30 19:10:25 +0100

    Set correct permissions on docker-compose

--- mint/bootstrap.sh
+++ mint/bootstrap.sh
@@ -24,6 +24,7 @@ sudo apt install -y apt-transport-https ca-certificates software-properties-comm
 sudo apt install -y docker.io
 sudo usermod -aG docker $USER
 sudo curl -L https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
 
 # shell & term
 sudo apt install -y zsh zsh-syntax-highlighting gnome-terminal
